04e3f59d5e887b405062016859429004b0e839ed
Luke Uritescu <Luritescu@gmail.com>
2019-11-17 20:50:30 -0600

    Completed part 6 of Razor Pages witth EFCore

---
+++ ContosoUniversity/ContosoUniversity/Pages/Courses/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public CreateModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["CourseID"] = new SelectList(_context.Courses, "CourseID", "CourseID");
+        ViewData["InstructorID"] = new SelectList(_context.Instructors, "ID", "FirstMidName");
+            return Page();
+        }
+
+        [BindProperty]
+        public CourseAssignment CourseAssignment { get; set; }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.CourseAssignments.Add(CourseAssignment);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ ContosoUniversity/ContosoUniversity/Pages/Courses/Edit.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.Pages.Courses
+{
+    public class EditModel : PageModel
+    {
+        private readonly ContosoUniversity.Data.SchoolContext _context;
+
+        public EditModel(ContosoUniversity.Data.SchoolContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public CourseAssignment CourseAssignment { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            CourseAssignment = await _context.CourseAssignments
+                .Include(c => c.Course)
+                .Include(c => c.Instructor).FirstOrDefaultAsync(m => m.CourseID == id);
+
+            if (CourseAssignment == null)
+            {
+                return NotFound();
+            }
+           ViewData["CourseID"] = new SelectList(_context.Courses, "CourseID", "CourseID");
+           ViewData["InstructorID"] = new SelectList(_context.Instructors, "ID", "FirstMidName");
+            return Page();
+        }
+
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(CourseAssignment).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CourseAssignmentExists(CourseAssignment.CourseID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool CourseAssignmentExists(int id)
+        {
+            return _context.CourseAssignments.Any(e => e.CourseID == id);
+        }
+    }
+}
